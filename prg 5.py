c6165131539cf1149e1811b60e68efa2f175ee2b
karanudani140632-prog <karan.udani140632@marwadiuniversity.ac.in>
2026-02-12 15:53:44 +0530

    Update prg 5.py

--- prg 5.py
+++ prg 5.py
@@ -1,15 +1,16 @@
 # 5. Write a function inside another function.
 
 
-def outer_function(x, y):
-    def inner_addition(a, b):
+def fun1(x, y):
+    def fun2(a, b):
         return a + b
     
-    result = inner_addition(x, y)
+    result = fun2(x, y)
     return result * 2  
 
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-print("Final result:", outer_function(num1, num2))
+print("Final result:", fun1(num1, num2))
+
